81b22273f5628f1d9928f4cbb1f5ce9df18c9b5a
Nguyen Thanh An <thanhan28122078@gmail.com>
2026-04-03 17:20:37 +0700

    Lab02

--- Lab02/AimsProject/src/Aims.java
+++ Lab02/AimsProject/src/Aims.java
@@ -1,5 +1,7 @@
 public class Aims {
     static void main() {
+       DigitalVideoDisc dvd1 = new DigitalVideoDisc(
+               "The Lion King", "Animation", "Roger Allers", 87, 19.95f);
 
     }
 }
---
+++ Lab02/AimsProject/src/Cart.java
@@ -0,0 +1,5 @@
+public class Cart {
+    public static final int MAX = 20;
+    private DigitalVideoDisc itemsOrdered[] =
+            new DigitalVideoDisc[MAX];
+}
