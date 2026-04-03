39bcd5d076811a605b913cb8889a6a79734e6d48
Nguyen Thanh An <thanhan28122078@gmail.com>
2026-04-03 17:00:27 +0700

    Lab02

---
+++ Lab02/AimsProject/src/Aims.java
@@ -0,0 +1,5 @@
+public class Aims {
+    static void main() {
+
+    }
+}
---
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,7 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+}
